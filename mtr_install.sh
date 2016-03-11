3855d193e9cda61c0001777694efcf3220320de4
xXGuillaumeXx <gcanavaggio@mozilla.com>
2016-03-11 18:06:45 +0100

    Update mtr_install.sh

--- mtr_install.sh
+++ mtr_install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-#Installs homebrew
+#Installs homebrew & Apple CLI tools if not already installed
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-#Adds mtr install directory to PATH
-sudo su root -c "echo '/usr/local/sbin' >> /etc/paths"
 
 #Installs mtr
 brew install mtr
+
+#Adds mtr install directory to PATH after user password entry
+sudo su root -c "echo '/usr/local/sbin' >> /etc/paths"
+
+echo -e "Please close the current terminal window, open a new one and type in:\nsudo mtr -r @IP\nwhere @IP is the target IP address"
+#end
